a4b55d4dd1ca675eeefc35da14025097de1c1636
Cichol <cichol@live.cn>
2014-05-16 16:20:00 +0800

    init

--- app.rb
+++ app.rb
@@ -1,13 +1,14 @@
 require 'sinatra'
+require 'sinatra/activerecord'
 require 'maruku'
+
+
+set :database, {adapter: "sqlite3", database: "development.sqlite3"}
 
 enable :sessions
 
 get "/admin" do 
   session[:verified] = true if params[:pass] == "mayflyorg"
   @session = session[:verified]? "verified : true" : "verified : false"
-  a = Maruku.new("abc")
-  a.to_html
   erb :admin
 end
-

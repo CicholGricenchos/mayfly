f60b353d9ef5c6b6cea94f13e421ae0d2d3a42c5
Cichol <cichol@live.cn>
2014-05-16 16:17:34 +0800

    init

--- app.rb
+++ app.rb
@@ -1,51 +1,13 @@
 require 'sinatra'
-require 'sinatra/activerecord'
 require 'maruku'
-require './models/article'
-require './models/user'
-require './models/category'
-
-set :database, {adapter: "sqlite3", database: "development.sqlite3"}
 
 enable :sessions
 
 get "/admin" do 
   session[:verified] = true if params[:pass] == "mayflyorg"
   @session = session[:verified]? "verified : true" : "verified : false"
+  a = Maruku.new("abc")
+  a.to_html
   erb :admin
 end
 
-post "/new_article" do 
-  if session[:verified]
-    Article.create(:title => params[:title], :content => params[:content], :brief => params[:brief])
-  end
-end
-
-get "/article/:id" do 
-  article = Article.find(params[:id])
-  @content = Maruku.new(article.content)
-  @content = @content.to_html
-  @title = article.title
-  @date = article.created_at
-  @author = User.find(article.author).name
-  @category = Category.find(article.category).name
-  @page_title = "蜉蝣人文爱好小组 - #{@title}"
-  @content = erb :article
-  erb :page
-end
-
-get '/' do
-  @page_title = "蜉蝣人文爱好小组"
-  articles = Article.all
-  @content = ""
-  articles.each do |x|
-    @id = x.id
-    @brief = x.brief
-    @title = x.title
-    @date = x.created_at
-    @author = User.find(x.author).name
-    @category = Category.find(x.category).name
-    @content += erb :index
-  end
-  erb :page
-end

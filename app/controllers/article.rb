582b52bf29bfa710f30c2bcd442374684abdb051
Cichol <cichol@live.cn>
2014-07-19 23:33:59 +0800

    fixed bugs

--- app/controllers/article.rb
+++ app/controllers/article.rb
@@ -6,6 +6,8 @@ Mayfly::App.controllers :article do
   get :index, :with => :id do 
     @nav = 'about' if params[:id]=='9'
     @article = Article.find(params[:id])
+    @page_title = "蜉蝣人文爱好小组 - #{@article.title}"
+    @meta_description = @article.brief
     render :index
   end
 
--- app/controllers/category.rb
+++ app/controllers/category.rb
@@ -6,8 +6,10 @@ Mayfly::App.controllers :category do
   get :index, :with => :id do 
     @nav = 'design' if params[:id]=='1'
     @page = params[:page].nil? ? 1 : params[:page].to_i
-    @max_page= Article.where(:category_id => params[:id]).count / 8 + 1
-    @articles = Article.order("id DESC").where(:category_id => params[:id]).limit(7).offset((@page-1)*7)
+    @category = Category.find(params[:id])
+    @max_page = @category.articles.count / 8 + 1
+    @articles = @category.articles.order("id DESC").where(:category_id => params[:id]).limit(7).offset((@page-1)*7)
+    @page_title = "蜉蝣人文爱好小组 - #{@category.name}"
     render :index
   end
 end
